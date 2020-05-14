d40ffa394f6f1395ea4c0481c04a0260d2886641
mjindal585 <jindalmohit64@gmail.com>
2020-05-14 18:25:03 +0530

    changes

--- models/User.js
+++ models/User.js
@@ -29,9 +29,9 @@ User.seedAdminUser = async () => {
         let users = await User.find();
         if (users.length > 0) return;
         const salt = encryption.generateSalt();
-        const hashedPass = encryption.generateHashedPassword(salt, 'Admin');
+        const hashedPass = encryption.generateHashedPassword(salt, 'admin@123');
         return User.create({
-            username: 'Admin',
+            username: 'admin',
             salt,
             hashedPass,
             roles: ['Admin'],
